24ae84f17188d4069308a3bc3220570d10b7e8bf
Fayzullayevvv <tell2537505@gmail.com>
2025-02-12 00:33:59 +0500

    first time upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import CategoryCard from './components/CategoryCard'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/categories' element={<CategoryCard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+import CategoryCard from '../components/CategoryCard'
+const Home = () => {
+  return (
+    <div className='bg-[#fafafa]'>
+      <Header />
+      <CategoryCard />
+    </div>
+  )
+}
+
+export default Home
